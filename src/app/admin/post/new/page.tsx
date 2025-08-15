b3a2a4a645b297a74a2f02262a490511aeeb0ad6
Vinicius Vasconcelos da Silva <vinicius.vasconcelos2501@gmail.com>
2025-08-14 21:20:07 -0300

    feat:manage post form

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,34 +1,7 @@
-import { Button } from '@/components/Button';
-import { InputCheckbox } from '@/components/InputCheckbox';
-import { InputText } from '@/components/InputText';
-import clsx from 'clsx';
-import { BugIcon, CheckIcon, CircleCheckIcon } from 'lucide-react';
+import { ManagePostForm } from '@/components/admin/ManagePostForm';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
-  return (
-    <form action=''>
-      <div className={clsx('flex', 'flex-col', 'gap-6')}>
-        <InputText labelText='Seu nome' placeholder={'Digite seu nome:'} />
-        <InputText labelText='Seu email' placeholder={'Digite seu Email:'} />
-        <InputText
-          labelText='Seu email'
-          placeholder={'Digite seu Email:'}
-          disabled
-        />
-
-        <InputCheckbox labelText='Checkbox Text' />
-      </div>
-
-      <div
-        className={clsx('py-16', 'flex', 'flex-wrap', 'gap-4', 'items-center')}
-      >
-        <Button variant='default' size='md'>
-          <CircleCheckIcon />
-          enviar
-        </Button>
-      </div>
-    </form>
-  );
+  return <ManagePostForm />;
 }
