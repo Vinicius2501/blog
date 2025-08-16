317c5ed50cb24234b932f1b1758429238bd5a40b
Vinicius Vasconcelos da Silva <vinicius.vasconcelos2501@gmail.com>
2025-08-16 11:30:30 -0300

    feat:upload imagens actions

--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,14 +1,16 @@
 'use client';
 
+import { uploadImageAction } from '@/actions/upload/upload-image-actions';
 import { Button } from '@/components/Button';
 import { IMAGEUPLOADER_MAX_SIZE } from '@/lib/constants';
 import clsx from 'clsx';
-import { ImageUpIcon } from 'lucide-react';
-import { useRef } from 'react';
+import { FileInput, ImageUpIcon } from 'lucide-react';
+import { useRef, useTransition } from 'react';
 import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isUploading, startTransition] = useTransition();
 
   function handleChooseFile() {
     if (!fileInputRef.current) return;
@@ -38,6 +40,17 @@ export function ImageUploader() {
 
     //TODO: Criar action para upload da imagem
     formData.append('file', file);
+
+    startTransition(async () => {
+      const result = await uploadImageAction(formData);
+
+      if (result.error) {
+        toast.error(result.error);
+        fileInput.value = '';
+        return;
+      }
+      toast.success(result.url);
+    });
 
     fileInput.value = '';
 
